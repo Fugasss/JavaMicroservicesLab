f9a526055a5bb0af04120adcaeb3d044921c96ad
Konstantin <krip.1.101@gmail.com>
2025-04-21 15:55:28 +0500

    Update

---
+++ services/race-service/src/test/java/com/javalab/raceservice/RaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalab.raceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
